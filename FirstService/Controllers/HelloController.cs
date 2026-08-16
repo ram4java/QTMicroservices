84bdea7bee6f974e94e2f6a8d53a908534024a75
Ram Shankar <ramshankar4java@gmail.com>
2026-08-16 11:55:42 +0530

    Removed WeatherForstcast feature in every api, addes specific controllers

---
+++ FirstService/Controllers/HelloController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        [Route("SayHello")]
+        public ActionResult SayHello()
+        {
+            return Ok("Hello Everyone");
+        }
+    }
+}
---
+++ SecondService/Controllers/GreetingsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SecondService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GreetUsers")]
+        public ActionResult GreetUsers()
+        {
+            return Ok("Welcome to Microservices Project");
+        }
+    }
+}
